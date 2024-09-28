ff722e13fd564465dd05f9fd7462c8e5ca6324ef
Zareen Hashmi <168464830+Zareenhashmi@users.noreply.github.com>
2024-09-28 22:52:20 +0530

    Add files via upload

---
+++ day1 (cricket worldcup qualifier).c.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int score;
+	cin>>score;
+	if(score>=12)
+	{
+	    cout<<"Yes";
+	}
+	else
+	{
+	    cout<<"No";
+	}
+}
