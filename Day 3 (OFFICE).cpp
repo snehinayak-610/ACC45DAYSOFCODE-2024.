e73733eb40ea807a5b8b85aca8e6e46b27a22efb
Zareen Hashmi <168464830+Zareenhashmi@users.noreply.github.com>
2024-09-30 22:25:43 +0530

    Add files via upload

---
+++ Day 3 (OFFICE).cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+		int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<(x*4) +y<<"\n";
+	    t--;
+	}
+	
+	// your code goes here
+	return 0;
+	
+}
+
+
