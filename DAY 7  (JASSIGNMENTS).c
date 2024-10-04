48352e54bd9a9c7996afcab237941333ce625d6e
Zareen Hashmi <168464830+Zareenhashmi@users.noreply.github.com>
2024-10-04 17:59:54 +0530

    Add files via upload

---
+++ DAY 7  (JASSIGNMENTS).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a;
+	    scanf("%d",&a);
+	    if(a+3<=10)
+	    {
+	        printf("Yes\n");
+	    }
+	    else
+	    {
+	        printf("No\n");
+	    }
+	}   
+	
+	    return 0;
+}
+
